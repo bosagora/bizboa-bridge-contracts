ba05cb63498283a8af6191b5b8cdddfd607831dc
michael.kim <michael.kim.200518@gmail.com>
2022-01-27 17:46:02 +0900

    initialize truffle

---
+++ truffle-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    },
+  },
+  compilers: {
+    solc: {
+      version: "0.5.0",
+      optimizer: {
+        enabled: true,
+        runs: 200
+      },
+      evmVersion: "byzantium"
+    }
+  }
+}
